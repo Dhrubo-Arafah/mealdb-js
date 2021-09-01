4fdd50d80a849b01058dc75e45708b39cf29e7bb
Samiul Arafah Dhrubo <66543604+Dhrubo-Arafah@users.noreply.github.com>
2021-09-01 19:31:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const allMeals = document.getElementById('meals')
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+
+
+button.addEventListener('click', e => {
+    e.preventDefault();
+    item = input.value.trim();
+    if (item == '') {
+        alert()
+    } else {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${item}`)
+            .then(res => res.json())
+            .then(data => {
+                meals = data.meals
+                console.log(meals)
+                if (meals === null) {
+                    alert()
+                }
+                else {
+                    allMeals.textContent = ''
+                    for (const meal of meals) {
+                        let item = Object.values(meal)
+                        newMeal(item[0], item[1])
+                    }
+                }
+            });
+            input.value='';
+    }
+})
+
+function alert() {
+    allMeals.innerHTML = `
+    <div class="alert alert-danger alert-dismissible fade show d-flex align-items-center mx-auto ps-3 pe-1 rounded" role="alert" >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+        </svg>
+        <div class="mx-auto w-100">
+            Invalid Input
+        </div>
+        <button type="button" class="btn rounded-circle mx-auto" data-bs-dismiss="alert"><i class="fas fa-times text-danger"></i></button>
+    </div>`
+}
+
+function newMeal(head, img) {
+    let singleMeal = document.createElement('div')
+    singleMeal.classList.add('col')
+    let inner = document.createElement('div')
+    inner.classList.add('shadow-sm', 'rounded', 'p-3', 'text-center')
+    let heading = document.createElement('h4')
+    heading.classList.add('mt-2')
+    heading.innerText = head
+    let image = document.createElement('img')
+    image.classList.add('w-100', 'rounded')
+    image.setAttribute('src', `${img}`)
+    inner.appendChild(image)
+    inner.appendChild(heading)
+    singleMeal.appendChild(inner)
+    allMeals.appendChild(singleMeal)
+}
